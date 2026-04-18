85b0b8565b28e000834d33413fcf50403bb2abf5
Ratmir <92830801+k-dozorov-k@users.noreply.github.com>
2026-04-18 06:51:06 +0400

    App update

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,22 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
 import Header from './components/Header.vue'
+import Hero from './components/Hero.vue'
+import Servers from './components/Servers.vue'
+import Sections from './components/Sections.vue'
 </script>
 
 <template>
   <div id="app">
     <Header />
+    <div id="main">
+      <Hero />
+      <Servers />
+      <Sections />
+    </div>
   </div>
 </template>
 
 <script setup></script>
 
-<style scoped>
-h1 {
-  color: #2c3e50;
-}
-</style>
+<style scoped></style>
